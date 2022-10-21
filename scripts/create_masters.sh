56ee17f5a5945050891970db201a1b49095359e2
antonmark <antonmark@gmail.com>
2022-10-21 15:52:12 -0400

    Added option to ignore overcommiting storage on the hypervisor.

--- scripts/create_masters.sh
+++ scripts/create_masters.sh
@@ -8,7 +8,7 @@ do
   virt-install --name="ocp4-${i}" --vcpus=4 --ram=32768 \
   --disk path=/var/lib/libvirt/images/ocp4-${i}.qcow2,bus=virtio,size=120 \
   --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio \
-  --graphics vnc,port=-1,listen=0.0.0.0 \
+  --graphics vnc,port=-1,listen=0.0.0.0 --check disk_size=off \
   --boot hd,network,menu=on --print-xml > ${WORK_DIR}/ocp4-$i.xml
   virsh define --file ${WORK_DIR}/ocp4-$i.xml
 done
--- scripts/create_odf.sh
+++ scripts/create_odf.sh
@@ -12,7 +12,7 @@ do
   virt-install --name="ocp4-${i}" --vcpus=12 --ram=28672 \
   --disk path=/var/lib/libvirt/images/ocp4-${i}.qcow2,bus=virtio,cache=none,format=qcow2,size=120 \
   --disk path=/var/lib/libvirt/images/odf-${i}.qcow2,bus=virtio,cache=none,format=qcow2,size=500 \
-  --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio --graphics vnc,listen=0.0.0.0 --noautoconsole \
+  --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio --graphics vnc,listen=0.0.0.0 --noautoconsole --check disk_size=off \
   --boot hd,network,menu=on --print-xml > ${WORK_DIR}/ocp4-$i.xml
   virsh define --file ${WORK_DIR}/ocp4-$i.xml
 done
