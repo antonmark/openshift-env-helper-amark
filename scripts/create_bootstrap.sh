f13f3698b15814a745a6ba886a422fdf69810f76
antonmark <antonmark@gmail.com>
2022-10-21 12:02:50 -0400

    Increased amount of ram and added VNC.

--- scripts/create_bootstrap.sh
+++ scripts/create_bootstrap.sh
@@ -4,9 +4,10 @@ WORK_DIR=../ocp4-workingdir
 
 for i in  bootstrap
 do
-  virt-install --name="ocp4-${i}" --vcpus=4 --ram=8192 \
+  virt-install --name="ocp4-${i}" --vcpus=4 --ram=16384 \
   --disk path=/var/lib/libvirt/images/ocp4-${i}.qcow2,bus=virtio,size=120 \
   --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio \
+  --graphics vnc,port=-1,listen=0.0.0.0 \
   --boot hd,network,menu=on --print-xml > ${WORK_DIR}/ocp4-$i.xml
   virsh define --file ${WORK_DIR}/ocp4-$i.xml
 done
--- scripts/create_masters.sh
+++ scripts/create_masters.sh
@@ -5,9 +5,10 @@ WORK_DIR=../ocp4-workingdir
 
 for i in master{0..2}
 do
-  virt-install --name="ocp4-${i}" --vcpus=4 --ram=12288 \
+  virt-install --name="ocp4-${i}" --vcpus=4 --ram=32768 \
   --disk path=/var/lib/libvirt/images/ocp4-${i}.qcow2,bus=virtio,size=120 \
   --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio \
+  --graphics vnc,port=-1,listen=0.0.0.0 \
   --boot hd,network,menu=on --print-xml > ${WORK_DIR}/ocp4-$i.xml
   virsh define --file ${WORK_DIR}/ocp4-$i.xml
 done
