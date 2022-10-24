726ac73d6d0ddc1b5089aa32b2d884e48f08c37e
antonmark <antonmark@gmail.com>
2022-10-24 15:59:41 -0400

    Added VNC.

--- scripts/create_workers.sh
+++ scripts/create_workers.sh
@@ -10,7 +10,7 @@ do
   DOMAIN=ocp4-worker${i}
   virt-install --name="${DOMAIN}" --vcpus=12 --ram=32768 \
   --disk path=/home/${DOMAIN}.qcow2,bus=virtio,size=120 \
-  --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio \
+  --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio --graphics vnc,listen=0.0.0.0 --noautoconsole --check disk_size=off \
   --boot hd,network,menu=on --print-xml > ${WORK_DIR}/${DOMAIN}.xml
   virsh define --file ${WORK_DIR}/${DOMAIN}.xml
 done
