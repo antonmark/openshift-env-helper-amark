b8a2545cf8e0512f250400c06940231e6670effd
antonmark <antonmark@gmail.com>
2023-01-30 13:58:32 -0500

    Password/username now taken interactively in terminal.

--- scripts/add-rhsm-to-ks.sh
+++ scripts/add-rhsm-to-ks.sh
@@ -1,7 +1,9 @@
+read -p "RHN Username: " USERNAME
+read -s -p "Password: " PASSWORD
 
-cat <<EOF >>  ${WORK_DIR}/helper-ks.cfg
+cat << EOF >>  ${WORK_DIR}/helper-ks.cfg
 %post --log=/root/registration_results.out
-subscription-manager register --auto-attach --username=${RHSM_USERNAME} --password=${RHSM_PASSWORD}
+subscription-manager register --auto-attach --username=${USERNAME} --password=${PASSWORD}
 subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
 %end
 EOF
