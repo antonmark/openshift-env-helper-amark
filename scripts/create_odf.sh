180e1718f6807ad95ae9c0c9d7f2ea46940e06ed
antonmark <antonmark@gmail.com>
2023-03-21 17:09:52 -0400

    Added memory to ODF VM's for more OSD's if needed. Minor Makefile change.

--- scripts/create_odf.sh
+++ scripts/create_odf.sh
@@ -9,7 +9,7 @@ WORK_DIR=./ocp4-workingdir
 
 for i in odf{0..2}
 do
-  virt-install --name="ocp4-${i}" --vcpus=12 --ram=28672 \
+  virt-install --name="ocp4-${i}" --vcpus=12 --ram=65536 \
   --disk path=/home/ocp4-${i}.qcow2,bus=virtio,cache=none,format=qcow2,size=120 \
   --disk path=/home/odf-${i}.qcow2,bus=virtio,cache=none,format=qcow2,size=500 \
   --os-variant rhel8.0 --network network=${NETWORK_NAME},model=virtio --graphics vnc,listen=0.0.0.0 --noautoconsole --check disk_size=off \
